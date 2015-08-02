1e3f330b0e338a64a0dc559f275c9c20859310b8
Stefan Müller <s.mueller@it.kls-glt.de>
2015-08-02 13:50:04 +0200

    Skeleton Dateien importiert

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <gtk/gtk.h>
-
-#include "xmz-mod-touch-app.h"
-
-int
-main (int arc, char *argv[])
-{
-    return g_application_run (G_APPLICATION (xmz-mod-touch_app_new ()), argc, argv);
-}
