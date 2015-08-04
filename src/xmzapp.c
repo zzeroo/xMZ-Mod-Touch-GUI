eec55942fdc4a5264dafffc8e7a776e5d665909a
Stefan Müller <s.mueller@it.kls-glt.de>
2015-08-04 14:57:25 +0200

    Vorschlag Wayland/ Weson Erkennung

--- src/xmzapp.c
+++ src/xmzapp.c
@@ -42,7 +42,8 @@ xmz_app_activate (GApplication *app)
                                     GDK_HINT_BASE_SIZE |
                                     GDK_HINT_MAX_SIZE);
 
-    if (TRUE)
+    char * wayland_display = getenv ("WAYLAND_DISPLAY");
+    if ( wayland_display != NULL)
         gtk_window_maximize (GTK_WINDOW (win));
 
     // set_decorated sollte verwendet werden.
