c0c8d8bca0094e9eec1f032b6cf3773de146e813
Stefan Müller <s.mueller@it.kls-glt.de>
2015-10-20 16:40:38 +0200

    Clean up

--- lib/capistrano/tasks/uptime.rake
+++
@@ -1,7 +0,0 @@
-desc "Report Uptimes"
-task :uptime do
-  on roles(:all) do |host|
-    #execute :any_command, "with args", :here, "and here"
-    info "Host #{host} (#{host.roles.to_a.join(', ')}):\t#{capture(:uptime)}"
-  end
-end
--- testdata.sh
+++
@@ -1,8 +0,0 @@
-sqlite3 database.db << EOF
-  INSERT INTO sensors VALUES (1, "Sensor 1", 0, 0, 0);
-  INSERT INTO sensors VALUES (2, "Sensor 2", 0, 0, 0);
-  INSERT INTO sensors VALUES (3, "Sensor 3", 0, 0, 0);
-  INSERT INTO sensors VALUES (4, "Sensor 4", 0, 0, 0);
-  INSERT INTO sensors VALUES (5, "Sensor 5", 0, 0, 0);
-  INSERT INTO sensors VALUES (6, "Sensor 6", 0, 0, 0);
-EOF
