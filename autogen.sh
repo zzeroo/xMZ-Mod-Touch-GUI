ae67528fa535adad66270858bfced8a8e525b08f
Stefan Müller <s.mueller@it.kls-glt.de>
2015-11-02 15:18:17 +0100

    Dysfunctional: Stop working on that for today

--- autogen.sh
+++ autogen.sh
@@ -7,13 +7,32 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
+#[ ! -f README ] && ln README.md README
+[ ! -f ChangeLog ] && touch ChangeLog
+[ ! -f NEWS ] && touch NEWS
+[ ! -f README ] && touch README
+[ ! -f AUTHORS ] && touch AUTHORS
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+        echo "*** No intltoolize found, please install the intltool package ***"
+        exit 1
+fi
+
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** No autoreconf found, please install it ***"
         exit 1
 fi
-autoreconf --install
+
+if test -z `which autopoint`; then
+        echo "*** No autopoint found, please install it ***"
+        exit 1
+fi
+
+
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
 
 cd "$olddir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
-
