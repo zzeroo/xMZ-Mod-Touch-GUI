632dbd72c3b93265658b4654c381ab58eea0c359
Stefan Müller <s.mueller@it.kls-glt.de>
2016-07-02 13:57:58 +0200

    Guard um das Modbus Device gebaut, Error fehlt noch

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,6 +1,7 @@
 use libmodbus_rs;
 use libmodbus_rs::modbus::Modbus;
 use std::env;
+use std::fs;
 
 pub struct Sensor {
     pub name: String,
@@ -22,20 +23,27 @@ impl Sensor {
             Ok(_) => "/dev/ttyS1",
             Err(_) => "/dev/ttyUSB0",
         };
-        let mut modbus = Modbus::new_rtu(device, 9600, 'N', 8, 1);
-        modbus.set_slave(self.modbus_slave_id);
-        let _ = modbus.set_debug(true);
-        let _ = modbus.rtu_set_rts(libmodbus_rs::MODBUS_RTU_RTS_DOWN);
-        let mut tab_reg: Vec<u16> = vec![];
-
-        match modbus.connect() {
-            Err(_) => { modbus.free(); }
+        // Wenn das Device vorhanden ist wird die Update Logic durchlaufen, ansonnsten nur ein
+        // Fehler aus gegeben.
+        match fs::metadata(device) {
             Ok(_) => {
-                tab_reg = modbus.read_registers(1, 1);
+                let mut modbus = Modbus::new_rtu(device, 9600, 'N', 8, 1);
+                modbus.set_slave(self.modbus_slave_id);
+                let _ = modbus.set_debug(true);
+                let _ = modbus.rtu_set_rts(libmodbus_rs::MODBUS_RTU_RTS_DOWN);
+                let mut tab_reg: Vec<u16> = vec![];
+
+                match modbus.connect() {
+                    Err(_) => { modbus.free(); }
+                    Ok(_) => {
+                        tab_reg = modbus.read_registers(1, 1);
+                    }
+                }
+                tab_reg.get(0).map(|var| self.adc_value = *var);
+                modbus.close();
+                modbus.free();
             }
+            Err(err) => { println!("Modbus Device: {} ist nicht erreichbar: {}", device, err); }
         }
-        tab_reg.get(0).map(|var| self.adc_value = *var);
-        modbus.close();
-        modbus.free();
     }
 }
