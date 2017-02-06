62dc31451382caac29f6c298174cc7c91d0adc75
Stefan Müller <s.mueller@it.kls-glt.de>
2017-02-06 12:06:29 +0100

    First Refactoring, remove unwrap(), noch nicht fertig

--- src/gui/gtk3/application.rs
+++ src/gui/gtk3/application.rs
@@ -50,8 +50,15 @@ fn poll_server_web_interface(server: Arc<Mutex<Server>>) -> Result<()> {
                     Err(err) => { debug!("{:?}", err) }
                     Ok(_) => {
                         {
-                            let mut server = server.lock().unwrap();
-                            *server = serde_json::from_str(&s).unwrap();
+                            match server.lock() {
+                                Ok(mut server) => {
+                                    match serde_json::from_str(&s) {
+                                        Ok(s) => { *server = s; }
+                                        Err(e) => {error!("Error: serde_json::from_str(&s) failed, {}", e)}
+                                    }
+                                }
+                                Err(e) => {error!("Error: Server.lock() failed, {}", e)}
+                            }
                         }
                     }
                 }
@@ -62,7 +69,7 @@ fn poll_server_web_interface(server: Arc<Mutex<Server>>) -> Result<()> {
     Ok(())
 }
 
-fn window_main_setup(window: &gtk::Window) {
+fn window_main_setup(window: &gtk::Window) -> Result<()> {
     let window_title = format!("{} {}",
                 env!("CARGO_PKG_DESCRIPTION"),
                 env!("CARGO_PKG_VERSION"));
@@ -70,18 +77,25 @@ fn window_main_setup(window: &gtk::Window) {
     window.set_title(&window_title);
     window.set_default_size(1024, 600);
 
-    let display = window.get_display().unwrap();
-    let screen = display.get_screen(0);
-    screen.set_resolution(130.0);
+    if let Some(display) = window.get_display() {
+        let screen = display.get_screen(0);
+        screen.set_resolution(130.0);
 
-    // CSS Datei einbinden
-    let css_style_provider = gtk::CssProvider::new();
-    let css_gui = include_str!("gui.css");
-    css_style_provider.load_from_data(css_gui).unwrap();
-    gtk::StyleContext::add_provider_for_screen(&screen, &css_style_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+        // CSS Datei einbinden
+        let css_style_provider = gtk::CssProvider::new();
+        let css_gui = include_str!("gui.css");
+        match css_style_provider.load_from_data(css_gui) {
+            Ok(_) => {
+                gtk::StyleContext::add_provider_for_screen(&screen, &css_style_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+            }
+            Err(e) => {error!("Error: css_style_provider.load_from_data() failed: {}", e)}
+        }
+    }
 
     #[cfg(not(feature = "development"))]
     window.fullscreen();
+
+    Ok(())
 }
 
 fn create_treestore(builder: &gtk::Builder, server: Arc<Mutex<Server>>) {
