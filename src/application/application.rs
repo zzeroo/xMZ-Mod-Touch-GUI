0a70dff4b527b640ee89d53294310b0e13be5465
zzeroo <co@zzeroo.com>
2017-05-30 16:45:01 +0200

    Live Update reborn

--- src/application/application.rs
+++ src/application/application.rs
@@ -50,8 +50,10 @@ fn update_server(server: &Arc<Mutex<XMZModTouchServer>>) {
     use hyper;
     use serde_json;
 
+    // let host = "localhost";
+    let host = "192.168.89.188";
     let client = hyper::Client::new();
-    if let Ok(mut response) = client.get("http://localhost:3000/api/v1").send() {
+    if let Ok(mut response) = client.get(&format!("http://{}:3000/api/v1", host)).send() {
         let mut s = String::new();
         match response.read_to_string(&mut s) {
             Err(e) => println!("Error: {}", e),
@@ -66,7 +68,7 @@ fn update_server(server: &Arc<Mutex<XMZModTouchServer>>) {
     }
 }
 
-fn create_treestore(builder: &gtk::Builder, server: Arc<Mutex<XMZModTouchServer>>) {
+fn create_treestore(builder: &gtk::Builder, server: Arc<Mutex<XMZModTouchServer>>) -> gtk::TreeStore {
     let treeview_kombisensors: gtk::TreeView   = build!(builder, "treeview_kombisensors");
     let treestore_kombisensors: gtk::TreeStore = build!(builder, "treestore_kombisensors");
 
@@ -74,29 +76,108 @@ fn create_treestore(builder: &gtk::Builder, server: Arc<Mutex<XMZModTouchServer>
         for (zone_id, zone) in server.get_zones().iter().enumerate() {
             let iter = treestore_kombisensors.append(None);
             treestore_kombisensors.set_value(&iter, 0, &glib::Value::from( &( (zone_id as i32) + 1 )) );
+            treestore_kombisensors.set_value(&iter, 7, &glib::Value::from( &(zone_id as i32) ));
 
-            for kombisensor in zone.get_kombisensors() {
+            for (kombisensor_id, kombisensor) in zone.get_kombisensors().iter().enumerate() {
                 let iter = treestore_kombisensors.append(Some(&iter));
                 treestore_kombisensors.set_value(&iter, 1, &glib::Value::from( &format!("{}", kombisensor.get_modbus_address()) ));
                 treestore_kombisensors.set_value(&iter, 2, &glib::Value::from( &format!("{}", kombisensor.get_kombisensor_type()) ));
                 treestore_kombisensors.set_value(&iter, 6, &glib::Value::from( &format!("{}", kombisensor.get_error_count()) ));
+                treestore_kombisensors.set_value(&iter, 7, &glib::Value::from( &(kombisensor_id as i32) ));
 
-                for sensor in kombisensor.get_sensors() {
+                for (sensor_id, sensor) in kombisensor.get_sensors().iter().enumerate() {
                     let iter = treestore_kombisensors.append(Some(&iter));
                     treestore_kombisensors.set_value(&iter, 2, &glib::Value::from( &format!("{}", sensor.get_sensor_type() )));
                     treestore_kombisensors.set_value(&iter, 3, &glib::Value::from( &format!("{:.02}", sensor.get_concentration() )));
                     treestore_kombisensors.set_value(&iter, 4, &glib::Value::from( &format!("{:.02}", sensor.get_concentration_average_15min() )));
                     treestore_kombisensors.set_value(&iter, 5, &glib::Value::from( &format!("{}", sensor.get_si() )));
+                    treestore_kombisensors.set_value(&iter, 7, &glib::Value::from( &(sensor_id as i32) ));
                 }
             }
         }
         treeview_kombisensors.expand_all();
+    }
+
+    treestore_kombisensors
+}
+
+fn update_treestore(builder: &gtk::Builder, server: &Arc<Mutex<XMZModTouchServer>>, treestore: &gtk::TreeStore) {
+    if let Ok(server) = server.lock() {
+        // Zone
+        if let Some(mut iter) = treestore.get_iter_first() {
+            let mut valid_zones = true;
+
+            while valid_zones {
+                // Get Zone Id
+                let zone_id = treestore.get_value(&iter, 7).get::<u32>();
+
+                // Kombisensor
+                if let Some(mut iter) = treestore.iter_children(Some(&iter)) {
+                    let mut valid_kombisensors = true;
+
+                    while valid_kombisensors {
+                        // Get Kombisensor Id
+                        let kombisensor_id = treestore.get_value(&iter, 7).get::<u32>();
+
+                        zone_id.map(|zone_id| {
+                            kombisensor_id.map(|kombisensor_id| {
+                                server.get_zone(zone_id as usize).map(|zone| {
+                                    zone.get_kombisensor(kombisensor_id as usize).map(|kombisensor| {
+                                        treestore.set_value(&iter, 1, &glib::Value::from( &format!("{}", kombisensor.get_modbus_address()) ));
+                                        treestore.set_value(&iter, 2, &glib::Value::from( &format!("{}", kombisensor.get_kombisensor_type()) ));
+                                        treestore.set_value(&iter, 6, &glib::Value::from( &format!("{}", kombisensor.get_error_count()) ));
+                                    });
+                                });
+                            });
+                        });
+
+                        // Sensor
+                        if let Some(mut iter) = treestore.iter_children(Some(&iter)) {
+                            let mut valid_sensors = true;
+
+                            while valid_sensors {
+                                // Get Sensor ID
+                                let sensor_id = treestore.get_value(&iter, 7).get::<u32>();
+
+                                zone_id.map(|zone_id| {
+                                    kombisensor_id.map(|kombisensor_id| {
+                                        sensor_id.map(|sensor_id| {
+                                            server.get_zone(zone_id as usize).map(|zone| {
+                                                zone.get_kombisensor(kombisensor_id as usize).map(|kombisensor| {
+                                                    kombisensor.get_sensor(sensor_id as usize).map(|sensor| {
+                                                        treestore.set_value(&iter, 2, &glib::Value::from( &format!("{}", sensor.get_sensor_type() )));
+                                                        treestore.set_value(&iter, 3, &glib::Value::from( &format!("{:.02}", sensor.get_concentration() )));
+                                                        treestore.set_value(&iter, 4, &glib::Value::from( &format!("{:.02}", sensor.get_concentration_average_15min() )));
+                                                        treestore.set_value(&iter, 5, &glib::Value::from( &format!("{}", sensor.get_si() )));
+                                                    });
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                                // println!("Sensor Type 2{:?}", treestore.get_value(&iter, 2));
+                                // println!("Sensor Concentration 3{:?}", treestore.get_value(&iter, 3));
+                                // println!("Sensor Average 4{:?}", treestore.get_value(&iter, 4));
+                                // println!("Sensor Si 5{:?}", treestore.get_value(&iter, 5));
+
+                                valid_sensors = treestore.iter_next(&mut iter);
+                            }
+                        }
+
+                        valid_kombisensors = treestore.iter_next(&mut iter);
+                    }
+                }
+
+                valid_zones = treestore.iter_next(&mut iter);
+            }
+        }
     }
 }
 
 pub fn launch() -> Result<()> {
     // Create a XMZModTouchServer in a Arc Mutex
     let server = Arc::new(Mutex::new(XMZModTouchServer::new()));
+    // Einmal den Server "von Hand" aktualisieren
     update_server(&server.clone());
 
     if gtk::init().is_err() {
@@ -140,10 +221,19 @@ pub fn launch() -> Result<()> {
     // Gtk main thread
     window_main.connect_delete_event(|_, _| {
         gtk::main_quit();
+
         Inhibit(false)
     });
 
-    create_treestore(&builder, server.clone());
+    let treestore_kombisensors = create_treestore(&builder, server.clone());
+
+    gtk::timeout_add(5000, clone!(builder, server, treestore_kombisensors => move || {
+        update_server(&server);
+        update_treestore(&builder, &server, &treestore_kombisensors);
+
+        ::glib::Continue(true)
+    }));
+
 
     // Show 2nd notebook tab
     let notebook_main: gtk::Notebook = build!(builder, "notebook_main");
